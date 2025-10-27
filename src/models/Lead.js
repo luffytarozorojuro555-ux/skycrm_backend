67d42891136febd243d74060d8b907e098af6bf9
CHEVULA SOMESH <146305649+Somesh2504@users.noreply.github.com>
2025-10-27 22:35:21 +0530

    Update Lead.js

--- src/models/Lead.js
+++ src/models/Lead.js
@@ -5,14 +5,27 @@ const LeadSchema = new mongoose.Schema({
   email: { type: String ,unique: true, sparse: true},
   city: { type: String },
   source: { type: String },
+  college: { type: String },               
+  yearOfPassout: { type: Number },   
+
   assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   teamId: { type: mongoose.Schema.Types.ObjectId, ref: 'Team' },
   status: { type: mongoose.Schema.Types.ObjectId, ref: 'Status', required: true },
+
   history: [{
     status: { type: mongoose.Schema.Types.ObjectId, ref: 'Status' },
     by: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     at: { type: Date, default: Date.now }
   }],
+
+  comments: [
+    {
+      text: { type: String, required: true },
+      by: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, 
+      at: { type: Date, default: Date.now }
+    }
+  ],
+
   notes: [{ type: String }],
   createdAt: { type: Date, default: Date.now }
 });
