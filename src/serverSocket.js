24ae9c409a50500699791812239d482288ba47bc
luffytarozorojuro555-ux <233588710+luffytarozorojuro555-ux@users.noreply.github.com>
2026-02-22 16:18:48 +0530

    Implement CORS for allowed origins in socket server

--- src/serverSocket.js
+++ src/serverSocket.js
@@ -1,15 +1,31 @@
 import { Server } from "socket.io";
 
 let io;
+const allowedOrigins = [
+  "https://skycrm.co.in",
+  "https://www.skycrm.co.in",
+  "http://localhost:5173"
+];
 
 export function initSocket(server) {
   io = new Server(server, {
     cors: {
-      origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+      origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
       methods: ["GET", "POST"],
       credentials: true
     }
   });
+
+  io.on("connection", (socket) => {
+    console.log("Client connected:", socket.id);
+  });
+}
 
   io.on("connection", (socket) => {
     console.log("Client connected:", socket.id);
